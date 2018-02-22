064c22b00752721bd217105a21fbe81bc593d14c
rafgonsi <33317987+rafgonsi@users.noreply.github.com>
2018-02-22 12:05:32 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@ int main() {
 
     srand(time(0)); // potrzebne do losowania
 
-    std::string adresPliku = "C:\\projektycpp\\ruchdrogowy\\input2.txt";
+    std::string adresPliku =
+            R"(C:\Users\rafcik\Desktop\fizyka\Programowanie\C++\Projekt ruch miejski\plik.txt)";
 
     Symulacja s(adresPliku);
     s.wypisz();
     s.rozpocznij();
-
+    
 }
